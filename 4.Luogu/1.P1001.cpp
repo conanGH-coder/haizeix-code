7c4fa14788f1dfc46b3da82112ee2e616b6ff150
conanGH-coder <hello@123.com>
2024-10-06 20:32:52 +0800

    Update 4.Luogu

---
+++ 4.Luogu/1.P1001.cpp
@@ -0,0 +1,16 @@
+/*************************************************************************
+	> File Name: 1.P1001.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: Sun 06 Oct 2024 08:29:21 PM CST
+ ************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    cout << a + b << endl;
+    return 0;
+}
