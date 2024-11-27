cf834998206c3e9dd29a15bc2c0356239f32d92c
conanGH-coder <kudosherlock@outlook.com>
2024-11-27 15:32:14 +0800

    update

--- 8.C/第五章代码/6.binary_search.c
+++ 8.C/第五章代码/6.binary_search.c
@@ -7,7 +7,26 @@
 
 #include <stdio.h>
 
-int main() {
+int binary_search(int l, int r, int target, int arr[]) {
+    int head = l, tail = r - 1, mid;
+    while (head <= tail) {
+        mid = (head + tail) >> 1;
+        if (arr[mid] == target) return mid;
+        else if (arr[mid] < target) tail = mid - 1;
+        else head = mid + 1;
+    }
+    return -1;
+}
 
+int main() {
+    int x, n;
+    scanf("%d", &n);
+    int arr[n + 5];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", arr + i);
+    }
+    while (~scanf("%d", &x)) {
+        printf("find %d at index %d\n", x, binary_search(0, n, x, arr));
+    }
     return 0;
 }
